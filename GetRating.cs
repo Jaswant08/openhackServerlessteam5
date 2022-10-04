66b31087a4b8912ef52ba469e11b79bdeaf56581
maddy <keerthi.m26@gmail.com>
2022-10-04 16:46:23 -0400

    Removed cosmos namespace

--- GetRating.cs
+++ GetRating.cs
@@ -7,7 +7,6 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
-using Microsoft.Azure.Cosmos;
 using System.Collections.Generic;
 
 namespace oh5.serverless
