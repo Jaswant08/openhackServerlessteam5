8e0c07dd5c5caa3d38ec798010061d94826559a1
Brad Yates <bradkarhu@gmail.com>
2022-10-04 14:21:17 -0500

    Initial code

---
+++ CreateRating.cs
@@ -0,0 +1,46 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace oh5.serverless
+{
+    public static class CreateRating
+    {
+        [FunctionName("CreateRating")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            string name = req.Query["name"];
+
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            dynamic data = JsonConvert.DeserializeObject(requestBody);
+            
+            int num = 0;
+            int.TryParse(data?.rating, out num);
+
+            Rating rating = new Rating
+            {
+                userId = data?.userId,
+                productId = data?.productId,
+                locationName = data?.locationName,
+                rating = num,
+                userNotes = data?.userNotes
+            };
+
+            string responseMessage = string.IsNullOrEmpty(name)
+                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
+                : $"Hello, {name}. This HTTP triggered function executed successfully.";
+
+            return new OkObjectResult(responseMessage);
+        }
+    }
+}
