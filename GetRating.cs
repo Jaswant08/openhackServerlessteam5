cf5e5facdd490115003b9496301157b2b528c672
vgeorge-fi <v.george@fi.com>
2022-10-05 07:58:19 -0700

    Modified GetRating.

--- GetRating.cs
+++ GetRating.cs
@@ -8,37 +8,47 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 using System.Collections.Generic;
+using Microsoft.Azure.Cosmos;
+using System.Linq;
 
 namespace oh5.serverless
 {
     public class GetRating
-    {       
+    {
         [FunctionName("GetRating")]
         public async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)] HttpRequest req,           
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)] HttpRequest req,
             ILogger log)
         {
             string ratingId = req.Query["ratingId"];
 
-            log.LogInformation("Get rating received request with ratingId : {0}", ratingId);            
+            log.LogInformation("Get rating received request with ratingId : {0}", ratingId);
 
-            if (ratingId == null || ratingId == String.Empty)
+            if (string.IsNullOrWhiteSpace(ratingId))
             {
-                return new NotFoundResult();
+                return new BadRequestObjectResult("Rating id cannot be empty.");
             }
 
-            var rating = new IceCreamRating()
-            {
-                Id = ratingId,
-                ProductId = "product 123",
-                Rating = "4",
-                Timestamp = DateTime.Now.ToLongDateString(),
-                LocationName = "DC",
-                UserId = "system",
-                UserNotes = "It's awesome"
-            };
+            // Use a data service to store the ratings information to the backend
+            CosmosClient cosmosClient = new CosmosClient(CosmosSettings.ConnectionString, CosmosSettings.Options);
+            Database database = await cosmosClient.CreateDatabaseIfNotExistsAsync(CosmosSettings.DatabaseId);
+            Container container = await database.CreateContainerIfNotExistsAsync(CosmosSettings.ContainerId, CosmosSettings.PartitionKeyPath);
+            QueryDefinition queryDefinition = new QueryDefinition("select * from c where c.id = @ratingId").WithParameter("@ratingId", ratingId);
 
-            return new OkObjectResult(rating);
+            IceCreamRating rating = null;
+            using (FeedIterator<IceCreamRating> iterator = container.GetItemQueryIterator<IceCreamRating>(queryDefinition))
+            {
+                if (iterator.HasMoreResults)
+                {
+                    var ratings = await iterator.ReadNextAsync();
+
+                    rating = ratings.SingleOrDefault();
+
+                }
+            }
+
+            return rating != null ? new OkObjectResult(rating) : new BadRequestObjectResult("Rating not found");
+
         }
     }
 }
